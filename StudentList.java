c122f4a3d8aaaa38feb522af51085419c2d78d13
Md. Khalid Hasan <shohag9438@gmail.com>
2021-12-06 10:23:51 +0600

    Refactors duplicate file read and write logic into methods

--- StudentList.java
+++ StudentList.java
@@ -2,54 +2,39 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 public class StudentList {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws  IOException {
 
 //		Check arguments
 		if(args.length!=1) {
 			showMessage();
 			return;
 		}
+		String studentsFile=loadStudents("students.txt");
 		if(args[0].equals("a")) {
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader fileStream = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt")));
-			String studentsFile = fileStream.readLine();
+			System.out.println("Loading data ...");
 			String students[] = studentsFile.split(", ");
 			for(String student : students) {
 				System.out.println(student);
 			}
-			} catch (Exception e){} 
 			System.out.println("Data Loaded.");
 		}
 		else if(args[0].equals("r")) {
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader fileStream = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt")));
-			String studentsFile = fileStream.readLine();
+			System.out.println("Loading data ...");
 			String students[] = studentsFile.split(", ");
 			Random arandomObject = new Random();
 			int randomNumber = arandomObject.nextInt(students.length);
 			System.out.println(students[randomNumber]);
-			} catch (Exception e){} 
 			System.out.println("Data Loaded.");			
 		}
 		else if(args[0].contains("+")){
-			System.out.println("Loading data ...");			
-			try {
-			BufferedWriter writerStream = new BufferedWriter(new FileWriter("students.txt", true));
+			System.out.println("Loading data ...");
 			String addString = args[0].substring(1);
-			writerStream.write(", " + addString);
-			writerStream.close();
-			} catch (Exception e){}
-							
+			updateStudetntList(addString, "students.txt");
 			System.out.println("Data Loaded.");	
 		}
 		else if(args[0].contains("?")) 
 		{
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader fileStream = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt")));
-			String studentsFile = fileStream.readLine();
+			System.out.println("Loading data ...");
 			String students[] = studentsFile.split(", ");
 			boolean done = false;
 			String searchString = args[0].substring(1);
@@ -59,15 +44,11 @@ public class StudentList {
 						done=true;
 				}
 			}
-			} catch (Exception e){} 
 			System.out.println("Data Loaded.");				
 		}
 		else if(args[0].contains("c")) 
 		{
-			System.out.println("Loading data ...");			
-			try {
-			BufferedReader fileStream = new BufferedReader(new InputStreamReader(new FileInputStream("students.txt")));
-			String studentsFile = fileStream.readLine();
+			System.out.println("Loading data ...");
 			char allCharacter[] = studentsFile.toCharArray();
 			boolean in_word = false;
 			int count=0;
@@ -84,7 +65,6 @@ public class StudentList {
 				}
 			}
 			System.out.println(count +" word(s) found ");
-			} catch (Exception e){} 
 			System.out.println("Data Loaded.");				
 		}
 		else {
@@ -93,5 +73,14 @@ public class StudentList {
 	}
 	static void showMessage() {
 		System.out.println("No argument or Invalid argument.");
+	}
+	static String loadStudents(String fileName) throws IOException{
+		BufferedReader fileStream = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+		return fileStream.readLine();
+	}
+	static void updateStudetntList(String addString, String fileName) throws IOException {
+		BufferedWriter writerStream = new BufferedWriter(new FileWriter(fileName, true));
+		writerStream.write(", " + addString);
+		writerStream.close();
 	}
 }
